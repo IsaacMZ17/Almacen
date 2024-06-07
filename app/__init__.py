4a8876b491b3afe596b1fa79bca49a1da2a3c151
Isaac Zarate <moraleseduardoIZ050817@gmail.com>
2024-06-06 21:02:03 -0600

    cambio 06-06-2024 - 09:02 PM

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from .routes import Resource
 from .routes import APIRoutes
+from .config import Config
+from .extensions import db
 
 def crear_app():
     app = Flask(__name__)
+
+    db.init_app(app)
+
+    app.config.from_object(Config)
+
     api = Api(app)
 
     routes= APIRoutes()
